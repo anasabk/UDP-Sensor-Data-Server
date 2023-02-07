0673ebc0b12d26cb64c2f5f5b44a5e91fb281683
Enes <67118653+anasabk@users.noreply.github.com>
2023-02-07 15:12:30 +0300

    Add files via upload

---
+++ src/test/java/com/iot/data_server/DataServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iot.data_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
